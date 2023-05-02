3041f3e7e552506df3711fb33b9e0f97865ff717
faysalewucse <faysal.ewucse@gmail.com>
2023-05-02 16:35:29 +0600

    chef card design done

--- src/components/ChefInfoCard.jsx
+++ src/components/ChefInfoCard.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import PrimaryButton from "./PrimaryButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as faHeartSollid } from "@fortawesome/free-solid-svg-icons";
 
 export default function ChefInfoCard({ info }) {
   return (
-    <div>
-      <img src={info.chef_picture} alt="" />
-      <div>
-        <h1>{info.chef_name}</h1>
-        <h1>{info.years_of_experience}</h1>
-        <h1>{info.number_of_recipes}</h1>
-        <h1>{info.likes}</h1>
+    <div className="flex gap-3 bg-white rounded-2xl">
+      <img
+        className="w-1/2 h-64 object-cover object-top rounded-l-2xl"
+        src={info.chef_picture}
+        alt=""
+      />
+      <div className="py-5 flex flex-col justify-between">
+        <div>
+          <h1 className="text-2xl font-bold">{info.chef_name}</h1>
+          <h1 className="text-lg">
+            Experience: {info.years_of_experience} Years
+          </h1>
+          <h1 className="text-lg">{info.number_of_recipes}</h1>
+          <h1 className="text-lg">{info.likes}</h1>
+        </div>
+        <div className="flex justify-center gap-3 items-center">
+          <PrimaryButton text={"View Recipes"} style={"w-48"} />
+          <FontAwesomeIcon icon={faHeart} className="text-2xl cursor-pointer" />
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/Chefs.jsx
+++ src/pages/Home/Chefs.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import ChefInfoCard from "../../components/ChefInfoCard";
 
 export default function Chefs({ chefs }) {
-  return <div>Chefs</div>;
+  return (
+    <div className="bg-orange-100 p-20">
+      <div className="max-w-screen-2xl mx-auto grid grid-cols-3 gap-5">
+        {chefs.map((chef) => (
+          <ChefInfoCard key={chef.id} info={chef} />
+        ))}
+      </div>
+    </div>
+  );
 }
