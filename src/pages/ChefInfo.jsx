b569674fba93bc5b0fabff793892adeb6f29f79e
faysalewucse <faysal.ewucse@gmail.com>
2023-05-04 06:25:57 +0600

    Spinner Added on CEhfInfoPage

--- src/pages/ChefInfo.jsx
+++ src/pages/ChefInfo.jsx
@@ -1,13 +1,15 @@
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import RecipeCard from "../components/RecipeCard";
 import ScrollToTop from "../utils/ScrollToTop";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { CirclesWithBar } from "react-loader-spinner";
 
 export default function ChefInfo() {
   const chefInfo = useLoaderData();
+  const navigation = useNavigation();
   const {
     id,
     chef_name,
@@ -25,55 +27,68 @@ export default function ChefInfo() {
     localStorage.setItem("favourite", JSON.stringify({ chefId, index }));
     toast.success(msg);
   };
+
   return (
     <div className="">
-      <div className="max-w-7xl mx-auto p-10">
-        {/* Banner */}
-        <div className="bg-gradient-to-tr to-orange-50 from-red-100 md:flex rounded-lg p-5">
-          <img
-            className="md:w-1/4 rounded-lg object-cover"
-            src={chef_picture}
-            alt="chef-image"
+      {navigation.state === "loading" ? (
+        <div className="flex justify-center items-center h-screen">
+          <CirclesWithBar
+            height="100"
+            width="100"
+            color="orange"
+            visible={true}
+            ariaLabel="circles-with-bar-loading"
           />
-          <div className="p-5 text-center md:text-left">
-            <h1 className="md:text-4xl text-2xl font-bold text-secondary2">
-              {chef_name}
-            </h1>
-            <h4 className="text-xl font-bold text-secondary">Experience</h4>
-            <p className="text-lg">{years_of_experience} years</p>
-            <h4 className="text-xl font-bold text-secondary">Recipes</h4>
-            <p className="text-lg">{number_of_recipes}</p>
+        </div>
+      ) : (
+        <div className="max-w-7xl mx-auto p-10">
+          {/* Banner */}
+          <div className="bg-gradient-to-tr to-orange-50 from-red-100 md:flex rounded-lg p-5">
+            <img
+              className="md:w-1/4 rounded-lg object-cover"
+              src={chef_picture}
+              alt="chef-image"
+            />
+            <div className="p-5 text-center md:text-left">
+              <h1 className="md:text-4xl text-2xl font-bold text-secondary2">
+                {chef_name}
+              </h1>
+              <h4 className="text-xl font-bold text-secondary">Experience</h4>
+              <p className="text-lg">{years_of_experience} years</p>
+              <h4 className="text-xl font-bold text-secondary">Recipes</h4>
+              <p className="text-lg">{number_of_recipes}</p>
 
-            <div className="flex gap-2 items-center mb-5 justify-center md:justify-start">
-              <FontAwesomeIcon
-                className="text-secondary2 text-xl"
-                icon={faThumbsUp}
-              />
-              <h1 className="text-lg text-secondary2 font-bold">{likes}</h1>
+              <div className="flex gap-2 items-center mb-5 justify-center md:justify-start">
+                <FontAwesomeIcon
+                  className="text-secondary2 text-xl"
+                  icon={faThumbsUp}
+                />
+                <h1 className="text-lg text-secondary2 font-bold">{likes}</h1>
+              </div>
+              <h4 className="inline-block border-b-2 border-secondary text-2xl font-bold text-secondary2">
+                Short Bio
+              </h4>
+              <p className="text-lg text-justify mt-3 text-secondary font-semibold">
+                {chefBio}
+              </p>
             </div>
-            <h4 className="inline-block border-b-2 border-secondary text-2xl font-bold text-secondary2">
-              Short Bio
-            </h4>
-            <p className="text-lg text-justify mt-3 text-secondary font-semibold">
-              {chefBio}
-            </p>
+          </div>
+          {/* Recipes */}
+          <div className="md:grid grid-cols-2 gap-5 py-10">
+            {recipes?.map((recipe, index) => {
+              return (
+                <RecipeCard
+                  chefId={id}
+                  index={index}
+                  key={index}
+                  recipe={recipe}
+                  notify={notify}
+                />
+              );
+            })}
           </div>
         </div>
-        {/* Recipes */}
-        <div className="md:grid grid-cols-2 gap-5 py-10">
-          {recipes?.map((recipe, index) => {
-            return (
-              <RecipeCard
-                chefId={id}
-                index={index}
-                key={index}
-                recipe={recipe}
-                notify={notify}
-              />
-            );
-          })}
-        </div>
-      </div>
+      )}
       <ToastContainer />
     </div>
   );
